a5c5d90daa643c5ef90e9d151b0c1fc79990247e
SushilKrishnan <sushilkrishnanvlr@gmail.com>
2023-11-17 13:15:45 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from keras.models import load_model
-from keras.models import load_model
+import keras.utils as image
 import numpy as np
 
 app = Flask(__name__)
